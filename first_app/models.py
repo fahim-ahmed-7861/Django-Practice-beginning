38ca029a7a240d6ee15738fdd23e0922218444de
fahim-ahmed-7861 <fahimahmedshojib109@gmai.com>
2021-04-18 16:45:22 +0600

    'done'

--- first_app/models.py
+++ first_app/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 
 
+
 class Musician(models.Model):
     #id=models.AutoField(primary_key=True)
     first_name = models.CharField(max_length=50)
@@ -11,11 +12,26 @@ class Musician(models.Model):
 
 
     def __str__(self):
-        return self.first_name
+        return self.first_name+" "+self.last_name;
 
 
 class Album(models.Model):
     artist = models.ForeignKey(Musician,on_delete=models.CASCADE)
     name= models.CharField(max_length=100)
     release_date=models.DateField()
-    num_stars=models.IntegerField()
+
+    ratting=(
+
+    (1,"worst"),
+    (2,"Bad"),
+    (3,"Not Bad"),
+    (4,"Good"),
+    (5,"Excellent"),
+
+
+    )
+    num_stars=models.IntegerField(choices=ratting)
+
+
+    def __str__(self):
+        return self.name
--- first_app/views.py
+++ first_app/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse
+from first_app.models import Musician,Album
 
 # Create your views here.
 
 def index(request):
-    diction={'text1':'i am a text sent from diction'}
+    musician_list=Musician.objects.order_by('first_name');
+
+    diction={'text1':'i am a text sent from diction',
+    'musician' : musician_list}
     return render(request,'first_app/index.html',context=diction);
 
 def contact(request):
